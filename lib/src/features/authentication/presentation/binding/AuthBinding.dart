e8530eb0ca0ceb7bada7d5db36ced8cda5aa783f
DevKorrir <kipyegonaldo@proton.me>
2025-07-31 22:57:29 +0300

    bind forgot password controller in the di

--- lib/src/features/authentication/presentation/binding/AuthBinding.dart
+++ lib/src/features/authentication/presentation/binding/AuthBinding.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_instance/src/bindings_interface.dart';
 
+import '../../../forgot_password/forgot_password_mail/ForgotPasswordController.dart';
 import '../../../screens/auth/login/login_controller.dart';
 import '../../controllers/sign_up_controller.dart';
 
@@ -10,5 +11,6 @@ class AuthBinding extends Bindings {
   void dependencies() {
     Get.lazyPut<SignUpController>(() => SignUpController());
     Get.lazyPut<LoginController>(() => LoginController());
+    Get.lazyPut<ForgotPasswordController>(() => ForgotPasswordController());
   }
 }
